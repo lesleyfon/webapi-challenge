4a6f7d1a1395f9c103459f8dcf29d8df279a5fff
lesley <lesleybanadzem2017@gmail.com>
2019-12-13 13:54:15 -0800

    Wrote endpoint to get project and post project to the db

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
+const projectRouter = require('./router/projectRouter');
+
 app.use(express.json());
 
 app.get('/', (req, res) => res.send('<h2> Welcome to web challenge api spritt<h2>'))
+
+app.use('/api/project', projectRouter);
+
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`))
---
+++ router/projectRouter.js
@@ -0,0 +1,38 @@
+const express = require('express');
+router = express.Router();
+const db = require('./../data/helpers//projectModel');
+
+
+router.get('/', async (req, res)=>{
+    try {
+        res.status(200).send(await db.get())
+    } catch (err){
+        res.status(404).json({
+            message: 'Error', 
+            error: err
+        })
+    }
+})
+router.post('/', async (req, res)=>{
+    const {name, description} = req.body;
+    if(!name || !description){
+        return res.status(500).json({
+            message: 'Please provide the required information for the request body'
+        });
+    }
+    try {
+        res.status(200).send(await db.insert({name, description}));
+    } catch (err){
+        res.status(404).json({
+            message: 'Error', 
+            error: err
+        })
+    }
+})
+
+
+
+
+
+
+module.exports= router
