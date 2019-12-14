b78ea3f855f1d78789da02e722e4336d9f52e02b
lesley <lesleybanadzem2017@gmail.com>
2019-12-13 17:39:54 -0800

    Update endpoint

--- router/actionRouter.js
+++ router/actionRouter.js
@@ -28,6 +28,41 @@ router.post('/',  validateAction(), async (req, res, next) =>{
         });
     }
 })
+router.delete('/:action_id',  async (req, res, next) =>{
+    const action_id = req.params.action_id;
+    try {
+        const actionToDelete = await db.remove(action_id);
+        (actionToDelete === 0) 
+            ?
+            res.status(500).json({message: ` Can't retrieve item at ${action_id}`})
+            :
+        res.status(200).json({
+            message: 'Successfully deleted',
+            delete: actionToDelete
+        })
+    } catch (error) {
+        res.status(400).json({
+            message: 'Error',
+            error: error
+        });
+    }
+})
 
+router.put('/:action_id',  validateAction(), async (req, res, next) =>{
+    const action_id = req.params.action_id;
 
+    try {
+        (!action_id) ?
+         res.status(500).json({
+            message: 'Please provide an id'
+            })
+            :
+         res.status(200).json( await db.update(action_id, req.action))
+    } catch (error) {
+        res.status(400).json({
+            message: 'Error',
+            error: error
+        });
+    }
+})
 module.exports = router;
