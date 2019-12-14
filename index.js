0093fc6c781beaac0b9e14101ff2c0da95c888f6
lesley <lesleybanadzem2017@gmail.com>
2019-12-13 17:52:52 -0800

    Installed cors to start building react app with the deployed api

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 require('dotenv').config()
 const express = require('express')
+const helmet = require('helmet');
+const cors = require('cors');
+
 const app = express()
 const PORT = process.env.PORT || 4000;
+app.use(helmet())
+app.use(cors())
+app.use(express.json());
 const projectRouter = require('./router/projectRouter');
 
-app.use(express.json());
+
 
 app.get('/', (req, res) => res.send('<h2> Welcome to web challenge api sprint<h2>'))
 
